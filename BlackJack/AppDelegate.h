065d3c00fcd94a08909be63b72a1efd77a1f2741
roseleaf <pteroline@gmail.com>
2012-08-10 11:11:28 -0700

    Initial Commit

---
+++ BlackJack/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  BlackJack
+//
+//  Created by Rose CW on 8/10/12.
+//  Copyright (c) 2012 Rose CW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ BlackJack/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BlackJack
+//
+//  Created by Rose CW on 8/10/12.
+//  Copyright (c) 2012 Rose CW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
